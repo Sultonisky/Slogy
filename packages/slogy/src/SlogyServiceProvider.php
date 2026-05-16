a1b685f06aa6d7ac537446761b71737b7346fa8e
sultonisky <muhsultonipml111@gmail.com>
2026-05-17 04:35:27 +0700

    feat: add activity logs migration support

--- packages/slogy/src/SlogyServiceProvider.php
+++ packages/slogy/src/SlogyServiceProvider.php
@@ -9,11 +9,22 @@ class SlogyServiceProvider extends ServiceProvider
         
     }
     public function boot(): void {
+        // config publish
         $this->publishes([
             __DIR__.'/../config/slogy.php'
-               => config_path('slogy.php'),
+                => config_path('slogy.php'),
         ], 'slogy-config');
 
+        // migrations publish (optional)
+        $this->publishes([
+            __DIR__.'/../database/migrations'
+                => database_path('migrations'),
+        ], 'slogy-migrations');
+
+        // auto load migrations
+        $this->LoadMigrationsFrom([
+            __DIR__.'/../database/migrations'
+        ]);
     }
 }
 
