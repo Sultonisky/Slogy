9608a0d8af3041877ea5931f8e59f7ee5d07ef5b
sultonisky <muhsultonipml111@gmail.com>
2026-06-11 20:05:07 +0700

    chore: Update playground to test Slogy newest features

--- playground/laravel-slogy-test/app/Models/Product.php
+++ playground/laravel-slogy-test/app/Models/Product.php
@@ -4,16 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Sultonisky\Slogy\Traits\HasSlogy;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Product extends Model
 {
 
-    use HasSlogy;
+    use HasSlogy, SoftDeletes;
 
     protected $table = 'products';
 
     protected $fillable = [
         'name',
     ];
+
+    public $slogyLabel = 'name';
 }
